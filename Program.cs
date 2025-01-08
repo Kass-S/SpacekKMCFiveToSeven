6fc0f2665d36359cedab9ed5ade6890f344a4bdb
Kass-S <kassiespacek101@gmail.com>
2025-01-08 14:50:49 -0800

    created controller and service for string reverse

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<MadLibServices>();
+builder.Services.AddScoped<StringReverseServices>();
 
 var app = builder.Build();
 
